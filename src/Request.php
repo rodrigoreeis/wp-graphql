e59236f9604fb6336ba800517b3a5f8f3a24c921
Jason Bahl <jasonbahl@mac.com>
2021-04-30 16:21:31 -0600

    - Add settings to restrict execution to v1.2.*

--- src/Request.php
+++ src/Request.php
@@ -8,9 +8,11 @@ use GraphQL\GraphQL;
 use GraphQL\Server\OperationParams;
 use GraphQL\Server\ServerConfig;
 use GraphQL\Server\StandardServer;
-use GraphQL\Validator\Rules\DisableIntrospection;
+use WPGraphQL\Server\ValidationRules\DisableIntrospection;
 use WP_Post;
 use WP_Query;
+use WPGraphQL\Server\ValidationRules\QueryDepth;
+use WPGraphQL\Server\ValidationRules\RequireAuthentication;
 use WPGraphQL\Server\WPHelper;
 use WPGraphQL\Utils\DebugLog;
 
@@ -178,13 +180,9 @@ class Request {
 
 		$validation_rules = GraphQL::getStandardValidationRules();
 
-		// If there is no current user and public introspection is not enabled, add the disabled rule to the validation rules
-		if ( ! get_current_user_id() && ! \WPGraphQL::debug() && 'off' === get_graphql_setting( 'public_introspection_enabled', 'off' ) ) {
-
-			$disable_introspection = new DisableIntrospection();
-			$validation_rules[]    = $disable_introspection;
-
-		}
+		$validation_rules[] = new RequireAuthentication();
+		$validation_rules[] = new DisableIntrospection();
+		$validation_rules[] = new QueryDepth();
 
 		/**
 		 * Return the validation rules to use in the request
